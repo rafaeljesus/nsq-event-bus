69e55c4bdbddd27789d677f1bc26fade3dc63cdd
Rafael Jesus <rafaelljesus86@gmail.com>
2017-01-15 20:03:56 -0200

    Release

---
+++ README.md
@@ -0,0 +1,26 @@
+## Event Bus NSQ
+
+* A tiny wrapper around [go-nsq](https://github.com/nsqio/go-nsq) topic and channel.
+
+## Usage
+
+TODO:
+
+## Contributing
+- Fork it
+- Create your feature branch (`git checkout -b my-new-feature`)
+- Commit your changes (`git commit -am 'Add some feature'`)
+- Push to the branch (`git push origin my-new-feature`)
+- Create new Pull Request
+
+## Badges
+
+[![Build Status](https://circleci.com/gh/rafaeljesus/nsq-event-bus.svg?style=svg)](https://circleci.com/gh/rafaeljesus/nsq-event-bus)
+[![Go Report Card](https://goreportcard.com/badge/github.com/rafaeljesus/nsq-event-bus)](https://goreportcard.com/report/github.com/rafaeljesus/nsq-event-bus)
+[![](https://images.microbadger.com/badges/image/rafaeljesus/nsq-event-bus.svg)](https://microbadger.com/images/rafaeljesus/nsq-event-bus "Get your own image badge on microbadger.com")
+
+---
+
+> GitHub [@rafaeljesus](https://github.com/rafaeljesus) &nbsp;&middot;&nbsp;
+> Medium [@_jesus_rafael](https://medium.com/@_jesus_rafael) &nbsp;&middot;&nbsp;
+> Twitter [@_jesus_rafael](https://twitter.com/_jesus_rafael)
---
+++ event_bus.go
@@ -0,0 +1,61 @@
+package event_bus
+
+import (
+	"encoding/json"
+	nsq "github.com/nsqio/go-nsq"
+	"os"
+)
+
+var NSQ_URL = os.Getenv("KYP_NSQ_URL")
+var NSQ_LOOKUPD_URL = os.Getenv("KYP_NSQ_LOOKUPD_URL")
+
+type fnHandler func(message []byte) error
+
+type EventBus interface {
+	Emit(topic string, message interface{}) error
+	On(topic, channel string, handler fnHandler) error
+}
+
+type Bus struct {
+	Producer *nsq.Producer
+	Config   *nsq.Config
+}
+
+func NewEventBus() (EventBus, error) {
+	config := nsq.NewConfig()
+	producer, err := nsq.NewProducer(NSQ_URL, config)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Bus{producer, config}, nil
+}
+
+func (bus *Bus) Emit(topic string, message interface{}) error {
+	payload, err := json.Marshal(message)
+	if err != nil {
+		return err
+	}
+
+	return bus.Producer.Publish(topic, payload)
+}
+
+func (bus *Bus) On(topic, channel string, handler fnHandler) error {
+	consumer, err := nsq.NewConsumer(topic, channel, bus.Config)
+	if err != nil {
+		return err
+	}
+
+	consumer.AddHandler(nsq.HandlerFunc(func(message *nsq.Message) error {
+		if err := handler(message.Body); err != nil {
+			return err
+		}
+		return nil
+	}))
+
+	if err := consumer.ConnectToNSQLookupd(NSQ_LOOKUPD_URL); err != nil {
+		return err
+	}
+
+	return nil
+}
