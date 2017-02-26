de34d220ad4e583e6e827cdcc5be2a42a2f455f8
Rafael Jesus <rafaelljesus86@gmail.com>
2017-02-26 03:15:46 -0300

    Make message a nsq.Message struct

--- message.go
+++ message.go
@@ -1,6 +1,18 @@
 package bus
 
+import (
+	"encoding/json"
+	nsq "github.com/nsqio/go-nsq"
+)
+
 type Message struct {
+	*nsq.Message
 	ReplyTo string
 	Payload []byte
+}
+
+func (m *Message) DecodePayload(v interface{}) (err error) {
+	err = json.Unmarshal(m.Payload, v)
+
+	return
 }
